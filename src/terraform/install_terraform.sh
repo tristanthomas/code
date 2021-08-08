a747e8dd8d6e392cfde77a75a8923f061b1a37d3
Tristan Thomas <tristho@gmail.com>
2021-08-08 22:08:22 +0000

    Configure Dev Container to use the Cloud SDK Docker image and install Terraform

---
+++ src/terraform/install_terraform.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+TERRAFORM_VERSION=1.0.4
+curl -L -o terraform_${TERRAFORM_VERSION}_linux_amd64.zip https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+mv terraform /usr/local/bin/
+chmod u+x /usr/local/bin/terraform
